994af23cb8807a7dcf9ab79e9b0de7f0701c5eb9
Glen Coakley <gcoakley@datarecognitioncorp.com>
2019-08-27 08:02:11 -0500

    Fixes for work laptop, incl. machine rename.

--- gac/interactive.bash
+++ gac/interactive.bash
@@ -7,12 +7,14 @@
 
 $GAC_CFG_DIR/ssh-agent-start.sh &
 
-echo -n "Interactive "
+[ "$GAC_DEBUG" -eq 1 ] && echo -n "Interactive "
 source_all shell-options.bash functions.bash aliases.bash \
         swdev/git.bash swdev/nodejs.bash swdev/npm-completion.bash
+source_optional prompt.bash
+source_optional completion.bash
 source_optional os-name/${GAC_OS_ABBR}
 source_optional machine/${GAC_MACHINE_ABBR}
-echo ''
+[ "$GAC_DEBUG" -eq 1 ] && echo ''
 
 # Enable directory-specific configurations using https://github.com/bmc/direnv
 eval `direnv hook $0`
